00970ec21ad64c66b7d3d49e9110106a5a116104
pourya ramezani <pourya.ramezani.21224@gmail.com>
2024-05-20 21:40:12 +0330

    The error value is proportional to the voltage value is coded

--- prj1.py
+++ prj1.py
@@ -99,27 +99,77 @@ oneVoltage = zeroVoltage * -1
 
 # signal = bit_string_maker(int(input("Enter signal length: ")))
 signal = bit_string_maker(20)
-print(signal)
+# print(signal)
 
 exp_len = 20
 signalExp = signal_expansion(signal, exp_len)
-signalExp = transform_to_voltage(signalExp,zeroVoltage,oneVoltage)
+signalExpV = transform_to_voltage(signalExp,zeroVoltage,oneVoltage)
 # print(signalExp)
 
-time = np.arange(len(signalExp))
+time = np.arange(len(signalExpV))
 
-plt.step(time,signalExp)
+plt.step(time,signalExpV)
 
-noisy_signal = communication_channel(signalExp)
+noisy_signal = communication_channel(signalExpV)
 
-print(noisy_signal)
+# print(noisy_signal)
 
 time = np.arange(len(noisy_signal))
 
 plt.plot(time,noisy_signal)
 
 samples = sampling(noisy_signal, len(signal), exp_len)
-print(samples)
+# print(samples)
 
 Retrieved_bits = Retrieving_bits_of_information(samples)
-print(Retrieved_bits)
+# print(Retrieved_bits)
+
+
+voltageArray = np.arange(0.1, 2.1, 0.1)
+percentage_of_error_bits = []
+
+for FzeroVoltage in voltageArray:
+    FsignalExp = signal_expansion(signal, exp_len)
+    FsignalExpV = transform_to_voltage(FsignalExp,FzeroVoltage,FzeroVoltage * -1)
+    Fnoisy_signal = communication_channel(FsignalExpV)
+    
+    
+    # time = np.arange(len(Fnoisy_signal))
+    # plt.figure()
+    # plt.step(time,FsignalExpV)
+    # plt.plot(time,Fnoisy_signal)
+    
+    Fsamples = sampling(Fnoisy_signal, len(signal), exp_len)
+    # print(Fsamples)
+
+    Retrieved_bits = Retrieving_bits_of_information(Fsamples)
+    # print(Retrieved_bits)
+    
+    # plt.figure()
+    # time = np.arange(len(Retrieved_bits))
+    # plt.step(time,Retrieved_bits)
+    
+    number_of_error_bits = 0
+
+    for i in range(len(signal)):
+        if(Retrieved_bits[i] != signal[i]):
+            number_of_error_bits += 1
+            pass
+        pass
+    
+    percentage_of_error_bits.append(number_of_error_bits / len(signal))
+    
+    pass
+
+plt.figure()
+time = np.arange(len(percentage_of_error_bits))
+plt.plot(time,percentage_of_error_bits)
+plt.title('The error value is proportional to the voltage value')
+
+plt.ylabel('Voltage')
+
+# افزودن شبکه (grid)
+plt.grid(True)
+
+# افزودن راهنما (legend)
+plt.legend()
